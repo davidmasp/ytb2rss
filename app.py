d53c9838bdf535e9a9d9ca442c04d415a8804181
David Mas-Ponte <david.mas.p@gmail.com>
2024-01-27 17:35:14 -0800

    adding a production server

--- app.py
+++ app.py
@@ -55,4 +55,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    # app.run(debug=True)
